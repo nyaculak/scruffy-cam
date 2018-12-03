e210f9d5e8648539ef6a1425d721a66df740f0e7
Nick Yaculak <nyaculak@gmail.com>
2018-12-02 19:54:48 -0500

    update server.py

--- server.py
+++ server.py
@@ -1,9 +1,10 @@
 import socket
 import subprocess
-
+import _thread
 from common import HOST, STREAM_PORT, DETECTOR_PORT
 
 stream_socket = socket.socket()
+stream_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 stream_socket.bind((HOST, STREAM_PORT))
 stream_socket.listen(0)
 stream_connection = stream_socket.accept()[0].makefile('rb')
@@ -16,25 +17,36 @@ detector_socket.listen()
 detector_connection, _ = detector_socket.accept()
 print("Detector connection established")
 
-try:
-    cmdline = ['vlc', '--demux', 'h264', '-']
-    player = subprocess.Popen(cmdline, stdin=subprocess.PIPE)
+def stream_thread():
+    global stream_socket, stream_connection
+    try:
+        cmdline = ['vlc', '--demux', 'h264', '-']
+        player = subprocess.Popen(cmdline, stdin=subprocess.PIPE)
 
+        while True:
+            stream_data = stream_connection.read(1024)
+            player.stdin.write(stream_data)
+    finally:
+        stream_connection.close()
+        stream_socket.close()
+        player.terminate()
+
+def detector_thread():
+    global detector_socket, detector_connection
+    try:
+        while True:
+            print("Reading detector data")
+            detector_data = detector_connection.recv(1024)
+
+            print("Writing to detector")
+            angle = "60"
+            detector_connection.sendall(str(angle).encode())
+    finally:
+        detector_connection.close()
+        detector_socket.close()
+
+if __name__ == "__main__":
+    _thread.start_new_thread(stream_thread, ())
+    _thread.start_new_thread(detector_thread, ())
     while True:
-        print("Reading stream data")
-        stream_data = stream_connection.read(1024)
-        player.stdin.write(stream_data)
-        
-        print("Reading detector data")
-        detector_data = detector_connection.recv(1024)
-
-        print("Writing to detector")
-        response = "60"
-        detector_connection.sendall(str(response).encode())           
-finally:
-    stream_connection.close()
-    stream_socket.close()
-    player.terminate()
-
-    detector_connection.close()
-    detector_socket.close()
+        pass
