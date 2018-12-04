c312652839baafe3c6a001d7f50e63ade4bb1f1c
jbowser1 <bowserjnb@gmail.com>
2018-12-03 22:30:52 -0500

    detector_client

--- detector_client.py
+++ detector_client.py
@@ -9,4 +9,22 @@ detector_connection = detector_socket.makefile('wb')
 print("Detector connection established")
 
 if __name__ == "__main__":
-    pass
+    try:
+        camera = picamera.PiCamera()
+        camera.resolution = (640, 480)
+        camera.framerate = 24
+        time.sleep(2)
+		
+		stream = io.BytesIO()
+        for foo in camera.capture_continuous(stream, 'jpeg'):
+            connection.write(struct.pack('<L', stream.tell()))
+            connection.flush()
+            stream.seek(0)
+            connection.write(stream.read())
+            stream.seek(0)
+            stream.truncate()
+    connection.write(struct.pack('<L', 0))
+    finally:
+        camera.stop_recording()
+        detector_connection.close()
+        detector_socket.close()
