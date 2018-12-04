872e9a346887b5b767c479b125db913acd9bca81
jbowser <bowserjnb@gmail.com>
2018-12-04 00:14:56 -0500

    switched to capture

--- detector_client.py
+++ detector_client.py
@@ -13,18 +13,20 @@ print("Detector connection established")
 if __name__ == "__main__":
     try:
         camera = picamera.PiCamera()
-        camera.resolution = (640, 480)
+        camera.resolution = (640,480)
         camera.framerate = 24
         time.sleep(2)
         
         stream = io.BytesIO()
-        for foo in camera.capture_continuous(stream, 'jpeg'):
+        while True:
+            camera.capture(stream, 'jpeg')
             detector_connection.write(struct.pack('<L', stream.tell()))
             detector_connection.flush()
             stream.seek(0)
             detector_connection.write(stream.read())
             stream.seek(0)
             stream.truncate()
+            time.sleep(10)
         detector_connection.write(struct.pack('<L', 0))
     finally:
         detector_connection.close()
