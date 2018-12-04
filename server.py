4d001a0e493fd15a08e97a66a5fcfb9e7ae45a48
Nick Yaculak <nyaculak@gmail.com>
2018-12-03 23:04:50 -0500

    adding detector socket to server

--- server.py
+++ server.py
@@ -1,3 +1,6 @@
+import io
+import struct
+from PIL import Image
 import socket
 import subprocess
 import _thread
@@ -20,11 +23,11 @@ controller_socket.listen()
 controller_connection, _ = controller_socket.accept()
 print("Controller connection established")
 
-detector_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+detector_socket = socket.socket()
 detector_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 detector_socket.bind((HOST, DETECTOR_PORT))
-detector_socket.listen()
-detector_connection, _ = detector_socket.accept()
+detector_socket.listen(0)
+detector_connection = detector_socket.accept()[0].makefile('rb')
 print("Detector connection established")
 
 angle = 0
@@ -40,6 +43,25 @@ def controller_thread():
     finally:
         controller_connection.close()
         controller_socket.close()
+
+def detector_thread():
+    global x
+    try:
+        while True:
+            image_len = struct.unpack('<L', detector_connection.read(struct.calcsize('<L')))[0]
+            if not image_len:
+                break
+
+            image_stream = io.BytesIO()
+            image_stream.write(detector_connection.read(image_len))
+            image_stream.seek(0)
+
+            image = Image.open(image_stream)
+            image.verify()
+            print('Image is %dx%d' % image.size)
+    finally:
+        detector_connection.close()
+        detector_socket.close()
 
 # https://raspberrypi.stackexchange.com/questions/34336/how-to-capture-keyboard-in-python
 def get_key_press():
@@ -67,12 +89,14 @@ def input_thread():
 if __name__ == "__main__":
     _thread.start_new_thread(controller_thread, ())
     _thread.start_new_thread(input_thread, ())
+    _thread.start_new_thread(detector_thread, ())
     try:
         cmdline = ['vlc', '--demux', 'mjpeg', '-']
         player = subprocess.Popen(cmdline, stdin=subprocess.PIPE)
         while True:
             stream_data = stream_connection.read(1024)
             player.stdin.write(stream_data)
+            continue
     finally:
         stream_connection.close()
         stream_socket.close()
